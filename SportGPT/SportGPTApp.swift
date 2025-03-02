7cc5f6d12c6f0e9915f8ef8dadd990a34489871b
keshew <artyom.korotckov2017@yandex.ru>
2025-03-02 18:08:46 +0500

    Initial Commit

---
+++ SportGPT/SportGPTApp.swift
@@ -0,0 +1,17 @@
+//
+//  SportGPTApp.swift
+//  SportGPT
+//
+//  Created by Артём Коротков on 02.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct SportGPTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
